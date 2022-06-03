a0b4b188e95aa4141041d35d257ef6b810b79128
Facundo Damian De Benedetti <facundodebenedetti09@hotmail.com>
2022-06-03 11:00:22 -0300

    primer commit

---
+++ BuscadorDeAlumnos/Alumno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuscadorDeAlumnos
+{
+    class Alumno
+    {
+        private string nombre;
+        private string apellido;
+        private int id;
+        private bool isActive;
+        private List<int> arregloNotas = new List<int>();
+        private int promNotas;
+
+        public Alumno(string nombre, string apellido, int id, bool isActive)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.id = id;
+            this.isActive = isActive;
+        }
+
+    }
+
+
+}
